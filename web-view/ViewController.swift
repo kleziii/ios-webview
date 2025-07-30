29dd453859276a56efaa84f0ee1db4322212d11a
kleziii <samuraj1345@gmail.com>
2025-07-30 21:25:44 +0200

    Update ViewController.swift

--- web-view/ViewController.swift
+++ web-view/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = NSURL (string: "https://apple.com");
+        let url = NSURL (string: "https://kacm.top/1322660847922843769");
         let requestObj = NSURLRequest(url: url! as URL);
         webView.loadRequest(requestObj as URLRequest);
         
